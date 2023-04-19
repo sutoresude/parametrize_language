5652031a04e9c43333f9f9200da153ff8f86bf93
a.kovalchuk <napstermfox@gmail.com>
2023-04-19 21:25:30 +0500

    parametriz_language

---
+++ test_items.py
@@ -0,0 +1,10 @@
+import time
+from selenium.webdriver.common.by import By
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+class TestProductPage:
+
+    def test_button_add_to_basket_is_visible(self, browser):
+        browser.get(link)
+        time.sleep(5)
+        assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
